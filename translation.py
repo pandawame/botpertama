12ab0fb9c9bddfc58d54ae0aac8e55d323989e9d
samtrp89 <94568902+samtrp89@users.noreply.github.com>
2022-02-05 18:26:29 +0800

    Update translation.py

--- translation.py
+++ translation.py
@@ -1,43 +1,58 @@
 class Translation(object):
 
-    START_TEXT = """
-Aku dapat melakukan banyak hal yang tidak dijelaskan oleh Penciptaku.
+    START_TEXT = """Hello,
 
-<b>Tidak semua orang dapat menjalankan fiturku!</b>
+This is a Telegram URL Upload Bot!
 
-Ketik /help untuk fitur dasar.
+<b>Please send me any Direct download URL link, I can upload to telegram as File/Video</b>
+
+/help for more details..
+
+Support Group : @TroJanzSupport
 """
 
-    HELP_USER = """
-1. Thumbnail bersifat permanen.
-   
-   <b>Fitur tangkapan layar secara default tidak diaktifkan oleh Penciptaku.
+    HELP_USER = """Hai I'am a URL Uploader bot..
+    
+1. Send url (Link | New Name with Extension).
+2. Send Custom Thumbnail (Optional).
+3. Select the button.
+   SVideo - Give File as video with Screenshots
+   DFile  - Give File with Screenshots
+   Video  - Give File as video without Screenshots
+   DFile  - Give File without Screenshots
 
-Untuk mengganti nama berkas kirim (Tautan | Nama Baru beserta Ekstensi).
-Untuk menghapus Thumbnail ketik /deletethumbnail
-Kamu hanya perlu mengirim Thumbnail baru untuk menimpanya!</b>
+Support Group : @TroJanzSupport
 """
 
-    FORMAT_SELECTION = """Pilih format yang diinginkan: <a href='{}'>perkiraan ukuran berkas</a>"""
-
-    DOWNLOAD_START = "Sedang mengunduh berkasmu..."
+    FORMAT_SELECTION = """Select the desired format: <a href='{}'>file size might be approximate</a>
     
-    UPLOAD_START = "Sedang mengunggah..."
+Send your custum thumbnail if required.
+You can use /deletethumbnail to delete the auto-generated thumbnail."""
     
-    AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = "Diundah dalam {} detik. \n\nDiunggah dalam {} detik."
+    SET_CUSTOM_USERNAME_PASSWORD = """If you want to download premium videos, provide in the following format:
+URL | newfilename | username | password"""
 
-    RCHD_TG_API_LIMIT = "Diunduh {} detik.\nUkuran Berkas Terdeteksi : {}\nMaaf. Tetapi, Aku tidak dapat mengunggah berkas yang lebih besar dari 1,95GB karena keterbatasan API Telegram."
 
-    SAVED_CUSTOM_THUMB_NAIL = "Thumbnail telah disimpan."
+    UPGRADE_TEXT = "<b>👉 Create own Clone Bot.. </b>  \n\n<a href='https://github.com/prgofficial/URLuploader-With-Hotstar'>Click here, Fork and deploy!!</a>"
+    
+    DOWNLOAD_START = "Trying to download your file..."
+    
+    UPLOAD_START = "Uploading now.."
+    
+    AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = "Downloaded in {} seconds. \n\nUploaded in {} seconds."
 
-    DEL_ETED_CUSTOM_THUMB_NAIL = "Thumbnail telah dihapus."
+    RCHD_TG_API_LIMIT = "Downloaded in {} seconds.\nDetected File Size: {}\nSorry. But, I cannot upload files greater than 1.95GB due to Telegram API limitations."
+
+    SAVED_CUSTOM_THUMB_NAIL = "Custom thumbnail saved. This will be permanent.\n\nUse /deletethumbnail to clear it."
+
+    DEL_ETED_CUSTOM_THUMB_NAIL = "Custom thumbnail cleared succesfully."
 
     CUSTOM_CAPTION_UL_FILE = " "
 
-    SLOW_URL_DECED = "Ya ampun, tampaknya berkas ini dihosting pada server yang sangat lambat, dan Aku tidak berminat untuk mengunduh berkas ini."
+    SLOW_URL_DECED = "Gosh that seems to be a very slow URL. Since you were screwing my home, I am in no mood to download this file. Meanwhile, why don't you try this:==> https://shrtz.me/PtsVnf6 and get me a fast URL so that I can upload to Telegram, without me slowing down for other users."
 
-    NO_VOID_FORMAT_FOUND = "ERROR...\n<b>YouTubeDL</b> mengatakan: {}"
+    NO_VOID_FORMAT_FOUND = "ERROR...\n<b>YouTubeDL</b> said: {}"
     
-    SHOW_THUMB = " "
+    SHOW_THUMB = "Use /deletethumbnail to clear this thumbnail."
     
-    NO_THUMB = " "    
+    NO_THUMB = "No saved thumbnails Found!!\n\nSend an image to save it as your thumbnail permanently."    
